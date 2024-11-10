9ca11047f6972117f221162c07e4de37402080ae
bonchardon <marynaboroda123@gmail.com>
2024-11-10 15:39:59 +0200

    updated consts.py

--- src/core/consts.py
+++ src/core/consts.py
@@ -2,3 +2,4 @@ from typing import Final
 
 URL_RESOURCE: Final[str] = ('https://raw.githubusercontent.com/'
                             'LetsData-net/datasets/main/datasets/publications_dataset.csv')
+BERT_MODEL: Final[str] = 'bert-base-uncased'
