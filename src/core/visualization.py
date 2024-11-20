9f15890e33f8fecc3b8e2da4d9b4397524802624
bonchardon <marynaboroda123@gmail.com>
2024-11-20 11:06:06 +0200

    current changes

--- src/core/visualization.py
+++ src/core/visualization.py
@@ -1,10 +1,13 @@
+import numpy as np
+
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
+
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 from loguru import logger
 
-from core.preprocess import Preprocess
+from text_similarity_algos.preprocess import Preprocess
 
 
 class Visualization:
@@ -27,3 +30,26 @@ class Visualization:
 
         except TypeError as e:
             logger.error(f'Error in words_visualisation: {e!r}')
+
+    @staticmethod
+    def visualize(distances, figsize=(10, 5), titles=None):
+        ncols = len(distances)
+        fig, ax = plt.subplots(ncols=ncols, figsize=figsize)
+
+        for i in range(ncols):
+            axes = ax[i] if ncols > 1 else ax
+            distance = distances[i]
+            axes.imshow(distance)
+            axes.set_xticks(np.arange(distance.shape[0]))
+            axes.set_yticks(np.arange(distance.shape[1]))
+            axes.set_xticklabels(np.arange(distance.shape[0]))
+            axes.set_yticklabels(np.arange(distance.shape[1]))
+            for j in range(distance.shape[0]):
+                for k in range(distance.shape[1]):
+                    text = axes.text(k, j, str(round(distance[j, k].item(), 3)),
+                                     ha="center", va="center", color="w")
+            title = titles[i] if titles and len(titles) > i else "Text Distance"
+            axes.set_title(title, fontsize="x-large")
+
+        fig.tight_layout()
+        plt.show()
--- src/main.py
+++ src/main.py
@@ -1,20 +1,18 @@
 from asyncio import run
 
-from core.preprocess import Preprocess
-from core.visualization import Visualization
-from core.text_similarity import TextSimilarity
+from text_similarity_algos.preprocess import Preprocess
+from text_similarity_algos.text_similarity import TextSimilarity
 
 
-async def main():
+async def main() -> None:
     text_analysis = Preprocess()
     result = await text_analysis.fully_preprocessed_data()
-    await Visualization().words_visualisation()
+    # await Visualization().words_visualisation()
 
-    text_similarity_check = TextSimilarity(data_texts=result)
-    response = await text_similarity_check.tf_idf_similarity_check()
+    text_similarity_check = TextSimilarity(data_texts=result['text'].to_list())
+    response = await text_similarity_check.bert_similarity()
     print(response)
 
 
 if __name__ == '__main__':
-
     run(main())
